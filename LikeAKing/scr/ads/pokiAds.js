85bdcd91a52791d6dfd99155b03f6b67fc1bd90d
ZRC-OUC <59873024+ZRC-OUC@users.noreply.github.com>
2022-02-10 00:48:29 +0800

    Add files via upload

---
+++ LikeAKing/scr/ads/pokiAds.js
@@ -0,0 +1,56 @@
+function initAdManager() {
+    debugLog("AD: init...");
+}
+
+function hasRewardedVideo(callback){
+    callback(true);
+}
+
+const REWARDED_AD_STATUS_FAILED = "rewardAdFailed";
+const REWARDED_AD_STATUS_REWARDED = "rewardAdDone";
+function showRewardedVideo(placement, callback) {
+    debugLog("AD: show rewarded ad...");
+    let inGame = gameplayActive;
+    trackStopGameplay();
+    PokiSDK.rewardedBreak().then(
+        (success) => {
+            if(inGame) {
+                trackStartGameplay();
+            }
+            if(success) {
+                // video was displayed, give reward
+                callback(REWARDED_AD_STATUS_REWARDED);
+            }
+            else {
+                // video not displayed, should probably not give reward
+                callback(REWARDED_AD_STATUS_FAILED);
+            }
+        }
+    );
+}
+
+function hasInterstitialVideo(callback){
+    callback(true);
+}
+
+const INTERSTITIAL_AD_STATUS_ENDED = "interstitialAdEnded";
+const INTERSTITIAL_AD_STATUS_FAILED = "interstitialAdFailed";
+function showInterstitialVideo(placement, callback) {
+    let inGame = gameplayActive;
+    trackStopGameplay();
+    PokiSDK.commercialBreak()
+        .then(() => {
+            debugLog("Commercial break finished, proceeding to game");
+            if(inGame) {
+                trackStartGameplay(true);
+            }
+            callback(INTERSTITIAL_AD_STATUS_ENDED);
+        })
+        .catch(() => {
+            debugLog("Commercial break error, proceeding to game");
+            if(inGame) {
+                trackStartGameplay(true);
+            }
+            callback(INTERSTITIAL_AD_STATUS_FAILED);
+        });
+}
